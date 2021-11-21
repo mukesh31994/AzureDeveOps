7063ef70f885d1b8b221d64bb8f4691c7e2247ca
unknown <mukeshsharma31994@gmail.com>
2021-11-21 17:59:32 +0000

    first commit

---
+++ src/main/java/com/java/azure/springboot/AzureDevOpsApplication.java
@@ -0,0 +1,26 @@
+package com.java.azure.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class AzureDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDevOpsApplication.class, args);
+	}
+	
+	@GetMapping("welcome")
+	public String getMessage() {
+		return "Welcome to Azure Web App Commit 3";
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "CICD successfully version 3";
+	}
+
+}
